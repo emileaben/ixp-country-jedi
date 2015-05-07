30b784f7e7f833acac81d2d053d99bdf07fcc9d0
Emile Aben <emileaben@gmail.com>
2015-05-07 17:15:30 +0200

    support for non-mesh measurements (target lists/alexa/list-from-website)

--- measure.py
+++ measure.py
@@ -10,45 +10,90 @@ from Atlas import Measure
 ### spit out measurementset.json
 
 probes=[]
+basedata = {}
 with open("probeset.json",'r') as infile:
    probes = json.load( infile )
+with open("basedata.json",'r') as infile:
+   basedata = json.load( infile )
 
 v6src=[] # src are probe IDs
 v4src=[]
-v4dst=[] # dst are IP addresses
-v6dst=[]
-
 for p in probes:
    if 'address_v4' in p and p['address_v4'] != None:
       v4src.append( p['probe_id'] )
-      v4dst.append( p['address_v4'] )
    if 'address_v6' in p and p['address_v6'] != None:
       v6src.append( p['probe_id'] )
-      v6dst.append( p['address_v6'] )
+
+v4dst=[] # dst are IP addresses
+v6dst=[]
+
+for mtype in basedata['measurement-types']:
+   if mtype == 'probe-mesh':
+      for p in probes:
+         if 'address_v4' in p and p['address_v4'] != None:
+            v4dst.append( p['address_v4'] )
+         if 'address_v6' in p and p['address_v6'] != None:
+            v6dst.append( p['address_v6'] )
+   elif mtype in ('http-traceroute','https-traceroute'):
+      v4dst += basedata['targets']
+      v6dst += basedata['targets']
 
 v4src_string = ','.join( map(str,v4src) )
 v6src_string = ','.join( map(str,v6src) )
 
 msms={'v4':[], 'v6':[]}
-for v4target in v4dst:
-   #TODO remove probe itself from list?
-   msm_id = Measure.oneofftrace(v4src, v4target, af=4, paris=1, description="ixp-country-jedi to %s (IPv4)" % ( v4target ) )
-   msms['v4'].append({
-      'msm_id': msm_id,
-      'dst': v4target
-   })
-   print >>sys.stderr,"dst:%s msm_id:%s" % ( v4target, msm_id )
-   time.sleep(2)
+for mtype in basedata['measurement-types']:
+   if mtype == 'probe-mesh':
+      for v4target in v4dst:
+         #TODO remove probe itself from list?
+         msm_id = Measure.oneofftrace(v4src, v4target, af=4, paris=1, description="ixp-country-jedi to %s (IPv4)" % ( v4target ) )
+         msms['v4'].append({
+            'msm_id': msm_id,
+            'dst': v4target,
+            'type': 'probe-mesh'
+         })
+         print >>sys.stderr,"dst:%s msm_id:%s (probe-mesh)" % ( v4target, msm_id )
+         time.sleep(2)
 
-v6msms=[]
-for v6target in v6dst:
-   msm_id = Measure.oneofftrace(v6src, v6target, af=6, paris=1, description="ixp-country-jedi to %s (IPv6)" % ( v6target ) )
-   msms['v6'].append({
-      'msm_id': msm_id,
-      'dst': v6target
-   })
-   print >>sys.stderr,"dst:%s msm_id:%s" % ( v6target, msm_id )
-   time.sleep(2)
+      for v6target in v6dst:
+         msm_id = Measure.oneofftrace(v6src, v6target, af=6, paris=1, description="ixp-country-jedi to %s (IPv6)" % ( v6target ) )
+         msms['v6'].append({
+            'msm_id': msm_id,
+            'dst': v6target,
+            'type': 'probe-mesh'
+         })
+         print >>sys.stderr,"dst:%s msm_id:%s (probe-mesh)" % ( v6target, msm_id, )
+         time.sleep(2)
+   if mtype in ('http-traceroute','https-traceroute'):
+      port = 80
+      if mtype == 'https-traceroute': port = 443
+      for target in v4dst: ## 
+         for ipproto in (4,6):
+            try:
+               ipstr = "v%s" % ( ipproto )
+               try:
+                  msm_id = Measure.oneofftrace(
+                     v4src,
+                     target,
+                     af=ipproto,
+                     paris=1,
+                     protocol='TCP',
+                     port=port,
+                     resolve_on_probe=True,
+                     description="ixp-country-jedi to %s (IPv%s)" % ( target, ipproto )
+                  )
+               except:
+                  print "measurement creation failed for dst:%s (IPv%s)" % ( target, ipproto )
+               msms[ ipstr ].append({
+                  'msm_id': msm_id,
+                  'dst': target,
+                  'type': mtype
+               })
+               print >>sys.stderr,"dst:%s msm_id:%s (%s)" % ( target, msm_id, mtype )
+               time.sleep(2)
+            except:
+               print "something went wrong"
 
+print >>sys.stderr, "finished measuring, writing results to 'measurementset.json'"
 with open("measurementset.json",'w') as outfile:
    json.dump(msms,outfile,indent=2)
