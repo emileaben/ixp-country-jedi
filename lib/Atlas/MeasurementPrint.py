3f816823d845ded6dec31acedbd281251a6a9af1
slowr <blackeye_l2@yahoo.gr>
2016-09-28 06:09:46 -0700

    fixed measurement print

--- lib/Atlas/MeasurementPrint.py
+++ lib/Atlas/MeasurementPrint.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import socket
+import urllib2
+import json
 import sys
 import arrow
 import IPInfoCache
@@ -12,66 +14,66 @@ ipcache=IPInfoCache.IPInfoCache()
 cache_only=False
 
 def IPInfoCacheFromFile( filename ):
-    ''' loads the IPinfo cache from file '''
-    global ipcache
-    ipcache.fromJsonFragments( filename )
+   ''' loads the IPinfo cache from file '''
+   global ipcache
+   ipcache.fromJsonFragments( filename )
 
 def setCacheOnly(flag):
-    ''' configures the IPInfo cache to not try for missing info '''
-    global cache_only
-    cache_only = flag
+   ''' configures the IPInfo cache to not try for missing info '''
+   global cache_only
+   cache_only = flag
 
 def _getips( data ):
-    ips = set()
-    for hop in data:
-        if not 'result' in hop:
-            continue
-        for hr in hop['result']:
-            if 'from' in hr and 'rtt' in hr:
-                if hr['from'] not in ips:
-                    ips.add( hr['from'] )
-    return ips
+   ips = set()
+   for hop in data:
+      if not 'result' in hop:
+         continue
+      for hr in hop['result']:
+         if 'from' in hr and 'rtt' in hr:
+            if hr['from'] not in ips:
+               ips.add( hr['from'] )
+   return ips
 
 def getipinfo( ip ):
-    ## returns a hash of IP info and caches the result
-    info = {}
-    if cache_only:
-        info = ipcache.getIPInfo(ip)
-    else:
-        info = ipcache.findIPInfo(ip)
-    return info
+   ## returns a hash of IP info and caches the result
+   info = {}
+   if cache_only:
+      info = ipcache.getIPInfo(ip)
+   else:
+      info = ipcache.findIPInfo(ip)
+   return info
 
 def trace2locs( data ):
-    locs = set()
-    msm_id = data['msm_id']
-    ips = _getips( data['result'] )
-    for ip in ips:
-        ipdata = getipinfo(ip)
-        loc = None
-        try:
-            loc = ipdata['location']
-            if loc != '' and loc != None:
-                locs.add( loc )
-        except: pass
-    return locs
+   locs = set()
+   msm_id = data['msm_id']
+   ips = _getips( data['result'] )
+   for ip in ips:
+      ipdata = getipinfo(ip)
+      loc = None
+      try:
+         loc = ipdata['location']
+         if loc != '' and loc != None:
+            locs.add( loc )
+      except: pass
+   return locs
 
 def trace2txt( data, **kwargs ):
-    '''
+   '''
       text representation of a raw json traceroute structure as text, annotating it with ASN, hostname, geoloc
       required arguments:
          data: traceroute data (Atlas json.loads for individual measurement)
       optional arguments:
          hostnames=False : don't look up hostnames
-    '''
-    txt = ''
-    res = data['result']
-    msm_id = data['msm_id']
-    print_hostnames = True
-    if 'hostnames' in kwargs:
-        print_hostnames = kwargs['hostnames']
-    ## print a header
-    tstring = arrow.get( data['timestamp'] ).format('YYYY-MM-DD HH:mm:ss ZZ')
-    txt += "## msm_id:%s prb_id:%s dst:%s ts:%s\n" % (msm_id, data['prb_id'], data['dst_addr'], tstring )
+   '''
+   txt = ''
+   res = data['result']
+   msm_id = data['msm_id']
+   print_hostnames = True
+   if 'hostnames' in kwargs:
+      print_hostnames = kwargs['hostnames']
+   ## print a header
+   tstring = arrow.get( data['timestamp'] ).format('YYYY-MM-DD HH:mm:ss ZZ')
+   txt += "## msm_id:%s prb_id:%s dst:%s ts:%s\n" % (msm_id, data['prb_id'], data['dst_addr'], tstring )
 
    for hop in res:
       ips = {}
@@ -83,27 +85,29 @@ def trace2txt( data, **kwargs ):
             if hr['from'] not in ips:
                ips[ hr['from'] ] = [ hr['rtt'] ]
             else:
-                err_set.add( "%s err:%s\n" % ( hop['hop'] , hr ) )
-        if len(err_set) > 0:
-            txt += '\n'.join(list(err_set))
+               ips[ hr['from'] ].append( hr['rtt'] )
+         else:
+            err_set.add( "%s err:%s\n" % ( hop['hop'] , hr ) )
+      if len(err_set) > 0:
+         txt += '\n'.join(list(err_set))
 
-        for ip in ips:
-            ipinfo = getipinfo(ip)
-            host = ''
-            try: host = ipinfo['hostname']
-            except: pass
-            asn = ''
-            try: asn = ipinfo['asn']
-            except: pass
-            loc = '' 
-            try: loc = ipinfo['location']
-            except: pass
-            #print "%s [AS%s] %s (%s) %s |%s| %s" % ( hop['hop'], asn, host, ip , sorted(ips[ip]), loc, type(loc) )
-            if asn != '':
-                asn = 'AS%s' % ( asn )
-            if print_hostnames == False or host == None or host == '':
-                host = ip
-            if loc == None:
-                loc = ''
-            txt += "%s (%s) %s %s |%s|\n" % ( hop['hop'], asn, host , sorted(ips[ip]), loc.encode('ascii','replace') )
-    return txt 
+      for ip in ips:
+         ipinfo = getipinfo(ip)
+         host = ''
+         try: host = ipinfo['hostname']
+         except: pass
+         asn = ''
+         try: asn = ipinfo['asn']
+         except: pass
+         loc = '' 
+         try: loc = ipinfo['location']
+         except: pass
+         #print "%s [AS%s] %s (%s) %s |%s| %s" % ( hop['hop'], asn, host, ip , sorted(ips[ip]), loc, type(loc) )
+         if asn != '':
+            asn = 'AS%s' % ( asn )
+         if print_hostnames == False or host == None or host == '':
+            host = ip
+         if loc == None:
+            loc = ''
+         txt += "%s (%s) %s %s |%s|\n" % ( hop['hop'], asn, host , sorted(ips[ip]), loc.encode('ascii','replace') )
+   return txt 
