e7da60976392d8f96fe0a8400736a33b9cd76e5f
Gigis <petros@unwired.local>
2016-12-19 14:23:41 +0200

    Zoom In-Out, Filter by ASNs, Share link functionality for ixpcountry visualization
    - Added zoom-in and zoom-out functionality for the visualization matrix.
    - Option for the display of ASNs.
    - Share link (Status of selected/unselected ASNs).

--- template/common/jedi-utils.js
+++ template/common/jedi-utils.js
@@ -26,3 +26,39 @@ function jedi_cell_detail_to_jquery_elt( proto, src_id , dst_id , jquery_elt ) {
       jquery_elt.html( txt );
    });
 }
+
+
+// Display the source and destination ASN of each cell
+function jedi_cell_show_source_dest_asn( proto, src_as , dst_as , source_asn, dest_asn, pageX, pageY) {
+
+
+        source_asn.style("display", "block");  //The tooltip appears
+        dest_asn.style("display", "block");  //The tooltip appears
+        
+        source_asn.html( "Source ASN: <b>" + src_as +"</b>"  )
+        .style("left", pageX - 220 + "px") 
+        .style("top", pageY - 15 + "px");
+
+        dest_asn.html( "Dest ASN: <b>" + dst_as +"</b>"  )
+        .style("left", pageX - 110 + "px") 
+        .style("top", pageY - 70 + "px");
+              
+}
+
+// Display the traceroute information
+function jedi_cell_show_traceroute_on_click( proto, src_id , dst_id , traceroute_detail, pageX, pageY) {
+
+
+  json_file = "../common/details/{0}/{1}/{2}/latest.json".format( proto, src_id, dst_id );
+  $.ajax({url: json_file, 
+    async: true
+  }).done(function( data ) {
+    var txt = '<button style="text-align:center" type="button" onclick="close_details(0)">Close</button> <pre>{0}</pre>'.format( data['tracetxt'] );
+    traceroute_detail.style("display", "block"); 
+
+    traceroute_detail.html( txt )
+      .style("left", (pageX + 20) + "px") 
+      .style("top", pageY + "px")
+  });
+              
+}
