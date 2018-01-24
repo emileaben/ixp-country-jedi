ed54a501b224e69c9ac13b9e8aa6b6d92a6e7793
Jasper den Hertog <jdenhertog@ripe.net>
2018-01-24 17:51:26 +0100

    minor adjustments

--- template/eyeballsgraph/app/index.js
+++ template/eyeballsgraph/app/index.js
@@ -3,100 +3,125 @@ import * as d3 from "d3";
 const SCALEFACTOR = 100;
 
 const getForceRadial = d =>
-    (d.type === "prb" && 210) || (d.type === "asn" && 140) || 70;
+  (d.type === "prb" && 210) || (d.type === "asn" && 140) || 70;
 
 const nodeClass = d =>
-    (d.type === "prb" && "prb") || (d.type === "asn" && "asn") || "";
+  (d.type === "prb" && "prb") || (d.type === "asn" && "asn") || "";
 
 d3.json("asgraph.json", function(error, data) {
-    console.log((error && error) || "loaded without errors");
-    console.log(data);
-    const ticked = () => {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
-        node
-            .attr("cx", function(d) {
-                return d.x;
-            })
-            .attr("cy", function(d) {
-                return d.y;
-            });
-    };
+  console.log((error && error) || "loaded without errors");
+  console.log(data);
+  const ticked = () => {
+    link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+    node
+      .attr("cx", function(d) {
+        return d.x;
+      })
+      .attr("cy", function(d) {
+        return d.y;
+      });
+  };
 
-    const svg = d3.select("svg");
+  function positionLink(d) {
+    return (
+      "M" +
+      d[0].x +
+      "," +
+      d[0].y +
+      "S" +
+      d[1].x +
+      "," +
+      d[1].y +
+      " " +
+      d[2].x +
+      "," +
+      d[2].y
+    );
+  }
 
-    var div = d3
-        .select("body")
-        .append("div")
-        .attr("class", "tooltip");
+  function positionNode(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  }
 
-    var link = svg
-        .append("g")
-        .selectAll(".link")
-        .data(data.edges)
-        .enter()
-        .append("line")
-        .attr("class", "link");
+  const svg = d3.select("svg");
 
-    var node = svg
-        .append("g")
-        .selectAll(".circle")
-        .data(data.nodes)
-        .enter()
-        .append("circle")
-        .attr("r", function(d) {
-            return Math.max(Math.log(d.count * SCALEFACTOR) * 3.5, 2);
-        })
-        .attr("class", nodeClass)
-        .on("mouseover", function(d) {
-            const g = d3.select(this);
-            div.style("opacity", 0.9);
-            div
-                .html(
-                    `<div class="tooltip"><h4>${d.name}</h5><p>${
-                        d.count
-                    }</p><div>`
-                )
-                .attr("left", `${d.x}px`)
-                .attr("top", `${d.y}px`);
-        })
-        .on("mouseout", function(d) {
-            div.style("opacity", 0);
-        });
+  var div = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip");
 
-    var nodes = data.nodes,
-        nodeById = d3.map(nodes, function(d) {
-            return d.id;
-        }),
-        links = data.edges,
-        bilinks = [];
+  var link = svg
+    //.append("g")
+    .selectAll(".link")
+    .data(data.edges)
+    .enter()
+    .append("line")
+    .attr("class", "link");
 
-    links.forEach(function(link) {
-        var s = (link.source = nodeById.get(link.source)),
-            t = (link.target = nodeById.get(link.target)),
-            i = {}; // intermediate node
-        nodes.push(i);
-        links.push({ source: s, target: i }, { source: i, target: t });
-        bilinks.push([s, i, t]);
+  var node = svg
+    .append("g")
+    .selectAll(".circle")
+    .data(data.nodes)
+    .enter()
+    .append("circle")
+    .attr("r", function(d) {
+      return Math.max(Math.log(d.count * SCALEFACTOR) * 3.5, 2);
+    })
+    .attr("class", nodeClass)
+    .on("mouseover", function(d) {
+      const g = d3.select(this);
+      div.style("opacity", 0.9);
+      div
+        .html(`<div class="tooltip"><h4>${d.name}</h5><p>${d.count}</p><div>`)
+        .attr("left", `${d.x}px`)
+        .attr("top", `${d.y}px`);
+    })
+    .on("mouseout", function(d) {
+      div.style("opacity", 0);
     });
 
-    //node.append("text").text(d => `${d.name} ${d.count}`);
-    // .append("text")
-    // .attr("")
+  var nodes = data.nodes,
+    nodeById = d3.map(nodes, function(d) {
+      return d.id;
+    }),
+    links = data.edges,
+    bilinks = [];
 
-    var simulation = d3
-        .forceSimulation()
-        .force("charge", d3.forceCollide().radius(8))
-        //.force("link", d3.forceLink(data.edges).distance(110))
-        .force("link", d3.forceLink(data.edges).distance(1250).strength(0.0001))
-        .force("r", d3.forceRadial(getForceRadial))
-        .nodes(data.nodes)
-        .on("tick", ticked);
+  links.forEach(function(link) {
+    var s = (link.source = nodeById.get(link.source)),
+      t = (link.target = nodeById.get(link.target)),
+      i = {}; // intermediate node
+    nodes.push(i);
+    links.push({ source: s, target: i }, { source: i, target: t });
+    bilinks.push([s, i, t]);
+  });
 
-    // var simulation = d3.forceSimulation().force("link", d3.forceLink().distance(1250).strength(0.001));
-    // simulation.nodes(nodes).on("tick", ticked);
-    // simulation.force("link").links(links);
+  //node.append("text").text(d => `${d.name} ${d.count}`);
+  // .append("text")
+  // .attr("")
+
+  var simulation = d3
+    .forceSimulation()
+    .force("charge", d3.forceCollide().radius(10))
+    //.force("link", d3.forceLink(data.edges).distance(110))
+    .force(
+      "link",
+      d3
+        .forceLink(data.edges)
+        .distance(1250)
+        .strength(0.00005)
+    )
+    .force("r", d3.forceRadial(getForceRadial))
+    .nodes(nodes)
+    .on("tick", ticked);
+
+    simulation.force("link").links(links);
+
+  // var simulation = d3.forceSimulation().force("link", d3.forceLink().distance(1250).strength(0.001));
+  // simulation.nodes(nodes).on("tick", ticked);
+  // simulation.force("link").links(links);
 });
